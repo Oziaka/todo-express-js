633b088e919bcecbb1e5a40bad346dc34d404a06
Oziaka <daniel.madizerski.pl@gmail.com>
2022-02-22 10:35:26 +0100

    Created base api

---
+++ model/Quest.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(id, title, description, isUrgent, isImportant) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.isUrgent = isUrgent;
+        this.isImportant = isImportant;
+    }
+}
